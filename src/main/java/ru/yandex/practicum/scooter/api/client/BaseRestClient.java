d22cea38ea26d52e8b2c572464312cdb52333b91
gasilyas <il.gasin@yandex.ru>
2026-08-15 11:05:46 +0300

    changing REST client structure using basic class

---
+++ src/main/java/ru/yandex/practicum/scooter/api/client/BaseRestClient.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.scooter.api.client;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class BaseRestClient {
+    protected static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+
+    protected RequestSpecification getBaseRequestSpecification() {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
--- src/main/java/ru/yandex/practicum/scooter/api/client/CourierRestClient.java
+++ src/main/java/ru/yandex/practicum/scooter/api/client/CourierRestClient.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.scooter.api.client;
 
-public class CourierRestClient {
-    private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+public class CourierRestClient extends BaseRestClient{
     private static final String COURIER_V1_PATH = "/api/v1/courier";
     private static final String COURIER_LOGIN_V1_PATH = "/api/v1/courier/login";
+
 }
