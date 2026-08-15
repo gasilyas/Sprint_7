dfa5e4772f0a8e145093a783c5aab76e989abc4d
gasilyas <il.gasin@yandex.ru>
2026-08-15 10:55:20 +0300

    rest clients' changes

--- src/main/java/ru/yandex/practicum/scooter/api/client/OrderRestClient.java
+++ src/main/java/ru/yandex/practicum/scooter/api/client/OrderRestClient.java
@@ -1,4 +1,6 @@
 package ru.yandex.practicum.scooter.api.client;
 
 public class OrderRestClient {
+    private static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
+    private static final String ORDER_V1_PATH = "/api/v1/order";
 }
